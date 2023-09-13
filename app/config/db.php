c04dfe33f2114929aaf2031825ae13a583836e70
avilovmaksim5 <avilovmaksim5@mail.ru>
2023-09-13 20:44:49 +0400

    update 13/09/23

--- app/config/db.php
+++ app/config/db.php
@@ -1,9 +1,17 @@
 <?php
 
+//return [
+//    'provider' => 'pgsql',
+//    'hostname' => 'localhost',
+//    'database' => 'frame',
+//    'username' => 'postgres',
+//    'password' => ''
+//];
+
 return [
     'provider' => 'pgsql',
-    'hostname' => 'localhost',
-    'database' => 'frame',
-    'username' => 'postgres',
-    'password' => ''
+    'hostname' => '194.190.152.134',
+    'database' => 'potok_8',
+    'username' => 'postgresql_potok8',
+    'password' => 'kyNV3j'
 ];
--- app/controllers/NewsController.php
+++ app/controllers/NewsController.php
@@ -21,7 +21,7 @@ class NewsController extends  InitController
                         }
                     ],
                     [
-                        'actions' => ['add'],
+                        'actions' => ['add', 'edit'],
                         'roles' => [UserOperations::RoleAdmin],
                         'matchCallback' => function() {
                             $this->redirect('/news/list');
@@ -65,6 +65,42 @@ class NewsController extends  InitController
 
         $this->render('add', [
             'sidebar' => UserOperations::getMenuLinks(),
+            'error_message' => $error_message
+        ]);
+    }
+    public function actionEdit()
+    {
+        $this->view->title = 'Редактирование новости';
+        $news_id = !empty($_GET['news_id']) ? $_GET['news_id'] : null;
+        $news = null;
+        $error_message = '';
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['btn_news_add_form'])) {
+            $news_data = !empty($_POST['news']) ? $_POST['news'] : null;
+
+            if (!empty($news_data)) {
+                $userModel = new NewsModels();
+                $result_edit = $userModel->edit($news_id, $news_data);
+                if ($result_edit['result']) {
+                    $this->redirect('/news/list');
+                } else {
+                    $error_message = $result_edit['error_message'];
+                }
+            }
+        }
+        if (!empty($news_id)) {
+            $news_model = new NewsModels();
+            $news = $news_model->getNewsById($news_id);
+            if (empty($news)) {
+                $error_message = 'Новость не найдена!';
+            }
+        } else {
+            $error_message = 'Отсутствует идентификатор записи!';
+        }
+
+        $this->render('edit', [
+            'sidebar' => UserOperations::getMenuLinks(),
+            'news' => $news,
             'error_message' => $error_message
         ]);
     }
